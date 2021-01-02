85c4fcf60f3e7188dd62aa45720ac0c46e7057f6
Guilherme Vier <guilhermevier456@gmail.com>
2021-01-02 16:37:36 -0300

    [Todas páginas] Refeito página do subcardápio com a pré-versão para PC, centralizado variáveis, corrigido erros da página inicial

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-const express = require('express');
+const express = require("express");
 const server = express();
 
-const {pageLanding,pageCardapio, pagesubCardapio, pagemontadorPizza, pageLogin} = require('./src/pages');
+const {
+  pageLanding,
+  pageCardapio,
+  pagesubCardapio,
+  pagemontadorPizza,
+  pageLogin,
+} = require("./src/pages");
 
-const nunjucks = require('nunjucks');
-nunjucks.configure('src/views', {
-	express: server,
-	noCache: true
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/views", {
+  express: server,
+  noCache: true,
 });
 
 server
-//receber os dados do req.body
-.use(express.urlencoded({ extended: true }))
-.use(express.static('public'))
-.get('/', pageLanding)
-.get('/cardapio',pageCardapio)
-.get('/subcardapio',pagesubCardapio)
-.get('/montadorpizza',pagemontadorPizza)
+  //receber os dados do req.body
+  .use(express.urlencoded({ extended: true }))
+  .use(express.static("public"))
+  .get("/", pageLanding)
+  .get("/cardapio", pageCardapio)
+  .get("/subcardapio", pagesubCardapio)
+  .get("/montadorpizza", pagemontadorPizza)
 
-.get('/login',pageLogin)
-.listen(5500);
+  .get("/login", pageLogin)
+  .listen(5500);
--- src/pages.js
+++ src/pages.js
@@ -1,22 +1,23 @@
-function pageLanding (req,res){
+function pageLanding(req, res) {
   return res.render("index.html");
 }
-function pageCardapio(req,res)
-{
+function pageCardapio(req, res) {
   return res.render("cardapio.html");
 }
-function pagesubCardapio(req,res)
-{
+function pagesubCardapio(req, res) {
   return res.render("subcardapio.html");
 }
-function pagemontadorPizza(req,res)
-{
+function pagemontadorPizza(req, res) {
   return res.render("montadorpizza.html");
 }
-function pageLogin(req,res){
+function pageLogin(req, res) {
   return res.render("login.html");
 }
 //Exporta os dados (paginas)
 module.exports = {
-  pageLanding, pageCardapio, pagesubCardapio, pagemontadorPizza, pageLogin
+  pageLanding,
+  pageCardapio,
+  pagesubCardapio,
+  pagemontadorPizza,
+  pageLogin,
 };
